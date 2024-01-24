e3064c7e73b8737ce3a16478958c8b557e89bc74
ajibola <yekinnijibola@gmail.com>
2024-01-24 20:47:15 +0100

    genesis:let there be light

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["media.istockphoto.com"], // Add the domains of your image sources
+  },
+};
 
 export default nextConfig;
