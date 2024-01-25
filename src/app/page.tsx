7a40e0d5ade6bbe759dd723b8a14cefad006e6b0
ajibola <yekinnijibola@gmail.com>
2024-01-25 12:56:35 +0100

    task: updated fonts and weight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="bg-[url(../../public/background.jpg)] pt-5 bg-cover bg-center h-screen flex justify-start items-center">
-      <div className="w-[85%] lg:w-[40%] p-[3.7rem] rounded-r-[2.5rem] text-white font-white bg-[#EA6668] opacity-80 h-[auto]">
+      <div className="w-[85%] lg:w-[50%] p-[1.5rem] lg:p-[3.7rem] rounded-r-[2.5rem] text-white font-white bg-[#EA6668] opacity-80 h-[auto]">
         <h1 className="text-[25px] lg:text-[65px]">Glome Health</h1>
         <p className="text-[18px] lg:text-[28px]">
           <i>Safer home dialysis for patients everywhere</i>
