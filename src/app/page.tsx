d1315dfb1ddc861acdbc5608852c01c099f301a7
ajibola <yekinnijibola@gmail.com>
2024-07-06 12:38:48 +0100

    task: added glome health mailchimp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { JSX, SVGProps } from "react";
 import { toast } from "sonner";
 
@@ -48,8 +48,50 @@ function SunIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+const MailchimpForm = () => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+      <!-- Replace with your Mailchimp form embed code -->
+        <div id="mc_embed_shell">
+      <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
+  <style type="text/css">
+        #mc_embed_signup{display:none;background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
+        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+    <form action="https://glomehealth.us17.list-manage.com/subscribe/post?u=5d796bf7f6b097112328b54f8&amp;id=0eeea179bd&amp;f_id=0080e9e3f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+        <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
+            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+            <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div>
+        <div id="mce-responses" class="clear foot">
+            <div class="response" id="mce-error-response" style="display: none;"></div>
+            <div class="response" id="mce-success-response" style="display: none;"></div>
+        </div>
+    <div aria-hidden="true" style="position: absolute; left: -5000px;">
+        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+        <input type="text" name="b_5d796bf7f6b097112328b54f8_0eeea179bd" tabindex="-1" value="">
+    </div>
+        <div class="optionalParent">
+            <div class="clear foot">
+                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe">
+                <p style="margin: 0px auto;"><a href="http://eepurl.com/iTh2aw" title="Mailchimp - email marketing made easy and fun"><span style="display: inline-block; background-color: transparent; border-radius: 4px;"><img class="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"></span></a></p>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script></div>
 
+      `,
+      }}
+    />
+  );
+};
 function Home() {
+  const mailChimpFormRef = useRef<HTMLFormElement>(null);
   const [isDarkMode, setIsDarkMode] = useState(false);
   let [email, setEmail] = useState("");
   const toggleDarkMode = () => {
@@ -57,32 +99,20 @@ function Home() {
     document.documentElement.classList.toggle("dark");
   };
 
-  const subscribe = async () => {
-    // Building the query string directly
-    const queryParams = new URLSearchParams({
-      EMAIL: email,
-      u: "83ab1dd4d6213d960c29718ba",
-      id: "f7544e59af",
-      f_id: "00c3c2e1f0",
+  const subscribe = () => {
+    const mailChimpInput = document.querySelector(
+      "#mce-EMAIL"
+    ) as HTMLInputElement;
+    const submit = document.querySelector(
+      "#mc-embedded-subscribe"
+    ) as HTMLInputElement;
 
-      b_83ab1dd4d6213d960c29718ba_f7544e59af: "",
-      subscribe: "Subscribe",
-      _: Date.now().toString(),
-    }).toString();
-
-    const url = `https://gmail.us13.list-manage.com/subscribe/post-json?${queryParams}`;
-
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      });
-
-      console.log(response);
-    } catch (error) {
-      console.error("Subscription failed:", error);
+    if (mailChimpInput && submit) {
+      mailChimpInput.value = email;
+      console.log("email is set: ", mailChimpInput?.value);
+      submit.click();
+      mailChimpFormRef.current?.reset();
+      toast.success("Subscribed successfully!");
     }
   };
 
@@ -139,6 +169,7 @@ function Home() {
             Get news, photos, events, and business updates
           </p>
           <form
+            ref={mailChimpFormRef}
             onSubmit={(e) => {
               e.preventDefault();
               subscribe();
@@ -169,6 +200,8 @@ function Home() {
           </form>
         </div>
       </div>
+
+      <MailchimpForm />
 
       <footer className=" text-sm pb-10 overflow-scroll w-full">
         <div className="flex justify-between w-[90%] md:w-[40%] mx-auto">
