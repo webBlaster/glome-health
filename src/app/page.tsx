452b7f8721b640f65012d6e916ab9bce5c1f62c7
ajibola <yekinnijibola@gmail.com>
2024-07-07 12:21:58 +0100

    task: fixed mailchimp subscribe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,71 +48,52 @@ function SunIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
-const MailchimpForm = () => {
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `
-      <!-- Replace with your Mailchimp form embed code -->
-        <div id="mc_embed_shell">
-      <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
-  <style type="text/css">
-        #mc_embed_signup{display:none;background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
-        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
-    <form action="https://glomehealth.us17.list-manage.com/subscribe/post?u=5d796bf7f6b097112328b54f8&amp;id=0eeea179bd&amp;f_id=0080e9e3f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-        <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
-            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-            <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div>
-        <div id="mce-responses" class="clear foot">
-            <div class="response" id="mce-error-response" style="display: none;"></div>
-            <div class="response" id="mce-success-response" style="display: none;"></div>
-        </div>
-    <div aria-hidden="true" style="position: absolute; left: -5000px;">
-        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
-        <input type="text" name="b_5d796bf7f6b097112328b54f8_0eeea179bd" tabindex="-1" value="">
-    </div>
-        <div class="optionalParent">
-            <div class="clear foot">
-                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe">
-                <p style="margin: 0px auto;"><a href="http://eepurl.com/iTh2aw" title="Mailchimp - email marketing made easy and fun"><span style="display: inline-block; background-color: transparent; border-radius: 4px;"><img class="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"></span></a></p>
-            </div>
-        </div>
-    </div>
-</form>
-</div>
-<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script></div>
 
-      `,
-      }}
-    />
-  );
-};
 function Home() {
   const mailChimpFormRef = useRef<HTMLFormElement>(null);
   const [isDarkMode, setIsDarkMode] = useState(false);
   let [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
     document.documentElement.classList.toggle("dark");
   };
 
-  const subscribe = () => {
-    const mailChimpInput = document.querySelector(
-      "#mce-EMAIL"
-    ) as HTMLInputElement;
-    const submit = document.querySelector(
-      "#mc-embedded-subscribe"
-    ) as HTMLInputElement;
+  const subscribeToNewsletter = async () => {
+    setLoading(true);
+    const url = "https://glomehealth.us17.list-manage.com/subscribe/post-json";
+    const params = new URLSearchParams({
+      u: "5d796bf7f6b097112328b54f8",
+      id: "0eeea179bd",
+      f_id: "0080e9e3f0",
+      c: "jQuery19005423992321011739_1720348739357",
+      EMAIL: email,
+      b_5d796bf7f6b097112328b54f8_0eeea179bd: "",
+      subscribe: "Subscribe",
+      _: new Date().getTime().toString(),
+    });
 
-    if (mailChimpInput && submit) {
-      mailChimpInput.value = email;
-      console.log("email is set: ", mailChimpInput?.value);
-      submit.click();
-      mailChimpFormRef.current?.reset();
-      toast.success("Subscribed successfully!");
+    try {
+      const response = await fetch(`${url}?${params.toString()}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        mode: "no-cors",
+      });
+
+      if (response) {
+        console.log(response);
+        toast.success("Subscribed successfully!");
+        setLoading(false);
+        if (mailChimpFormRef.current) {
+          mailChimpFormRef.current.reset();
+        }
+      }
+    } catch (error) {
+      console.error("Failed to subscribe:", error);
+      toast.error("Failed to subscribe. Please try again later.");
+      setLoading(false);
     }
   };
 
@@ -164,15 +145,15 @@ function Home() {
 
       <div className="mx-auto w-[90%] md:w-[40%]">
         <div className="text-center leading-3">
-          <p className=" mb-4 text-left font-bold dark:">Keep up with us</p>
-          <p className=" mb-4 text-left">
+          <p className="ml-5 mb-4 text-left font-bold dark:">Keep up with us</p>
+          <p className="ml-5 mb-4 text-left">
             Get news, photos, events, and business updates
           </p>
           <form
             ref={mailChimpFormRef}
             onSubmit={(e) => {
               e.preventDefault();
-              subscribe();
+              subscribeToNewsletter();
             }}
             className={`flex justify-center rounded-full border-2 p-[2px] ${
               isDarkMode
@@ -191,29 +172,36 @@ function Home() {
             />
             <button
               type="submit"
+              disabled={loading}
               className={`rounded-full md:px-8 py-2 text-[10px] md:text-[16px] md:py-4 w-[35%] ${
                 isDarkMode ? "bg-white text-primary" : "bg-primary text-white"
               }`}
             >
-              SIGN UP
+              {loading ? "...." : "SIGN UP"}
             </button>
           </form>
         </div>
       </div>
-
-      <MailchimpForm />
 
       <footer className=" text-sm pb-10 overflow-scroll w-full">
         <div className="flex justify-between w-[90%] md:w-[40%] mx-auto">
           <div className="w-[30%]">
             <h3 className="font-bold text-[16px] mb-2">ABOUT</h3>
             <p>
-              Glome Health is a UMN startup developing products & services to
-              make home dialysis more accessible for patients everywhere.
+              Glome Health is a{" "}
+              <a
+                className="italic"
+                href="https://research.umn.edu/units/techcomm/startups/university-startups"
+                target="_blank"
+              >
+                UMN startup
+              </a>{" "}
+              developing products & services to make home dialysis more
+              accessible for patients everywhere.
             </p>
           </div>
           <div>
-            <h3 className="font-bold text-[16px] mb-2">LOCATIONS</h3>
+            <h3 className="font-bold text-[16px] mb-2">LOCATION</h3>
             <p>
               336 Lilac Ln,
               <br />
